35044902151eed448616d2c1d579dd5dd87019af
Alessandro Ladu <ale1a1@libero.it>
2023-07-23 15:33:07 +0100

    slight style tweaks to header

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,17 @@ const Header = () => {
   return (
     <Fragment>
       <header className="vh-100 headerBackground">
-        <div className="container mt-5 pt-5 h-25">
-          <h4 className="text-light font-monospace text-shadow animate-text-right mt-4">
-            Financial Planing For Life
-          </h4>
-          <h1 className="display-2  h1Header text-shadow animate-text-right-3s mt-4">
-            Trusted to help guide entrepreneurs make better financial decisions
-          </h1>
+        <div className="container headerMainDiv">
+          <div className="text-light font-monospace text-shadow animate-text-right">
+            <span className="rounded subtitle">
+              Financial Planing For Life
+            </span>
+          </div>
+          <div className="d-flex display-3 text-light text-shadow rounded animate-text-right-3s">
+             <span className="title col-6 ps-3 rounded">
+              Trusted to help guide entrepreneurs make better financial decisions
+             </span>
+          </div>
           <button
             className="btn btn-lg btn-info shadow mt-4 animate-text-right-5s cssBold"
             data-bs-toggle="modal"
