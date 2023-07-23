905f8164e47bc7e44b29d419ed4773572c25da81
Alessandro Ladu <ale1a1@libero.it>
2023-07-23 16:20:26 +0100

    slight tweaks to header colors

--- src/Header.js
+++ src/Header.js
@@ -7,14 +7,14 @@ const Header = () => {
     <Fragment>
       <header className="vh-100 headerBackground">
         <div className="container mt-5 pt-5 h-25">
-          <h4 className="text-light font-monospace text-shadow animate-text-right mt-4">
+          <h4 className="text-light font-monospace text-shadow animate-text-right mt-4 ps-3 cssBold">
             Financial Planing For Life
           </h4>
-          <h1 className="display-2  h1Header text-shadow animate-text-right-3s mt-4">
+          <h1 className="display-2 rounded animate-text-right-3s mt-4 ps-3">
             Trusted to help guide entrepreneurs make better financial decisions
           </h1>
           <button
-            className="btn btn-lg btn-info shadow mt-4 animate-text-right-5s cssBold"
+            className="btn btn-lg btn-info shadow mt-4 animate-text-right-5s cssBold ms-3"
             data-bs-toggle="modal"
             data-bs-target="#getOffer"
           >
